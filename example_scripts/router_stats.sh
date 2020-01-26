98ffcfd5664a3c2de5505b614ee09da8a0541eea
rudoyeugene@gmail.com <Scienide83>
2020-01-26 18:12:04 +0200

    better scripts samples

--- example_scripts/router_stats.sh
+++ example_scripts/router_stats.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-ROUTER_CPU_TEMP=`sshpass -p admin ssh -o "StrictHostKeyChecking=no" admin@192.168.1.1 "cat /proc/dmu/temperature | sed 's/..$/ C/g'" | awk '{print $4}'`
-ROUTER_UPTIME=`sshpass -p admin ssh -o "StrictHostKeyChecking=no" admin@192.168.1.1 "uptime -p"`
 
+function router_free_ram_raw (){
+sshpass -p admin ssh -o "StrictHostKeyChecking=no" admin@192.168.1.1 "free | grep Mem"
+}
+
+function router_free_ram_kb (){
+FREE_RAM_KB=`router_free_ram_raw | awk '{print $4}'`
+echo $(($FREE_RAM_KB/1024))
+}
+
+ROUTER_CPU_TEMP=`sshpass -p admin ssh -o "StrictHostKeyChecking=no" admin@192.168.1.1 "cat /sys/class/thermal/thermal_zone0/temp | sed 's/.\{3\}$/.&/'"`
+ROUTER_UPTIME=`sshpass -p admin ssh -o "StrictHostKeyChecking=no" admin@192.168.1.1 "uptime -p"`
+FREE_RAM=`router_free_ram_kb`
+
+
+echo -e "\n"
+echo "Router statistics:"
 echo "Router uptime: $ROUTER_UPTIME"
-echo "Router CPU temperature: $ROUTER_CPU_TEMP °C"
+echo "Router CPU temperature: $ROUTER_CPU_TEMP°C"
+echo "Router free RAM: $FREE_RAM MB"
--- example_scripts/server_stats.sh
+++ example_scripts/server_stats.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-power_stats () {
+collect_power_stats () {
 upsc apc 2>&1 | grep -v '^Init SSL'> /tmp/ups_status
 }
 
-power_stats
+collect_power_stats
+
+VOLTS=V
 
 BOARD_UPTIME=`uptime -p`
 BOARD_TEMP=`sensors | grep -A 1 ACPI | grep -v ACPI | awk '{ print $2 }'`
@@ -14,19 +16,24 @@ FREE_STORAGE=`df -h | grep storage | awk '{print $4}'`
 HDD_TEMP=`hddtemp /dev/sda | awk '{ print $4 }'`
 
 #Power stats
-BATTERY_LEVEL=`cat /tmp/ups_status | egrep 'battery.charge' | egrep -v 'SSL' | sed 's/battery.charge/Battery level/' | awk '{ print $3 }'`
-INPUT_VOLTAGE=`cat /tmp/ups_status | egrep 'input.voltage' | egrep -v 'input.voltage.fault' | egrep -v 'SSL' | sed 's/input.voltage/Line voltage/' | awk '{ print $3 }'`
-BATTERY_LOAD=`cat /tmp/ups_status | egrep 'ups.load' | egrep -v 'SSL' | sed 's/ups.load/UPS load/' | awk '{ print $3 }'`
+BATTERY_LEVEL=`cat /tmp/ups_status | egrep 'battery.charge' | sed 's/battery.charge/Battery level/' | awk '{ print $3 }'`
+INPUT_VOLTAGE=`cat /tmp/ups_status | egrep 'input.voltage:' | sed 's/input.voltage/Line voltage/' | awk '{ print $3 }'`
+BATTERY_LOAD=`cat /tmp/ups_status | egrep 'ups.load' | sed 's/ups.load/UPS load/' | awk '{ print $3 }'`
+BATTERY_TEMP=`cat /tmp/ups_status | egrep 'ups.temperature' | sed 's/ups.temperature/UPS temperature/' | awk '{ print $3 }'`
 
-echo "Server uptime: $BOARD_UPTIME"
+echo "Board statistics:"
+echo "Board uptime: $BOARD_UPTIME"
 echo "Board temperature: $BOARD_TEMP"
 echo "CPU temperature: $CPU_TEMP"
 echo "Free RAM: $FREE_RAM MB"
 echo "Used swap: $USED_SWAP MB"
+echo -e "\n"
+echo "Storage statistics:"
 echo "Free storage: $FREE_STORAGE"
 echo "HDD temperature: $HDD_TEMP"
 echo -e "\n"
 echo "Power statistics:"
 echo "Battery level: $BATTERY_LEVEL%"
-echo "Input voltage: $INPUT_VOLTAGE V"
+echo "Input voltage: $INPUT_VOLTAGE$VOLTS"
 echo "Battery load: $BATTERY_LOAD%"
+echo "Battery temperature: $BATTERY_TEMP°C"
